13662702279d5bb0f96ebcc39c8d7d19c8dafaec
Triet Le <trietle847@gmail.com>
2025-02-12 00:33:18 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors =  require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-eror");
 
 const app = express();
 
@@ -10,6 +11,16 @@ app.use("/api/contacts",contactsRouter);
 
 app.get("/", (req,res) => {
     res.json({message: "Welcome to contact book application."});
+});
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 5000).json({
+        message: err.message || "Internall Server Error",
+    });
 });
 
 module.exports = app;
