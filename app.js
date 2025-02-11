82fffd7b69425366539be3ca6c51e6e1ab9717b0
Triet Le <trietle847@gmail.com>
2025-02-12 00:21:37 +0700

    dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors =  require("cors");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts",contactsRouter);
 
 app.get("/", (req,res) => {
-    req.join({message: "Welcome to contact book application."});
+    res.json({message: "Welcome to contact book application."});
 });
 
 module.exports = app;
